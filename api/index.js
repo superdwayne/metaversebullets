11097330291ae9aacaa03dbfe6bcbd5952aaef3f
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-11-16 09:34:03 +0000

    Removed old method of serving static files

--- api/index.js
+++ api/index.js
@@ -132,17 +132,4 @@ app.get('/xrtoday', function (req, res) {
     
 })
 
-
-
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../public')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public', 'index.html'));
-    });
-  }
-  
-
-
 app.listen(PORT, () => console.log(`Sever is running on PORT ${PORT}`))

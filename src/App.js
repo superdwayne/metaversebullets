2ffea8bd02f23b82eaa3781bef09a768644c0647
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-11-17 17:24:36 +0000

    reverted front end change

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function Articles()  {
 
     if (loading.current === true) {
 
-      Request(`http://localhost:5000/api`, params, (response) => {
+      Request(`/api`, params, (response) => {
         
         let shape = response
 

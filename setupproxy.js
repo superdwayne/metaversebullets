11b66ebf77c580a7942e486cd5fd145563e5cbb6
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-11-18 18:02:16 +0000

    Idea test number 2

--- setupproxy.js
+++ setupproxy.js
@@ -4,5 +4,4 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use('/api/**', createProxyMiddleware({ target: 'https://metaversebullets.vercel.app/', changeOrigin: true }));
-app.use('/theverge/**', createProxyMiddleware({ target: 'https://metaversebullets.vercel.app/', changeOrigin: true }));
 app.listen(5000);

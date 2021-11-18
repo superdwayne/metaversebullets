c56fba9587421eb4d95196665875054913a8647a
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-11-18 10:41:02 +0000

    Added proxy

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function Articles()  {
     console.log("Component mounting..",  loading )
 
 
-      Request(`http://localhost:5000/api`, params, (response) => {
+      Request(`/api`, params, (response) => {
         
         let shape = JSON.parse(JSON.stringify(response))
 
